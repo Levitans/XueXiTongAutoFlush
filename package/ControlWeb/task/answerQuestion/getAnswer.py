2cb8f71fdcc0c56903e734635452fe83b74ad380
Levitan <Levitan1165@outlook.com>
2022-02-11 10:33:28 +0800

    新增一个查题api

--- package/ControlWeb/task/answerQuestion/getAnswer.py
+++ package/ControlWeb/task/answerQuestion/getAnswer.py
@@ -10,15 +10,35 @@ import json
 
 class GetAnswer:
     def __init__(self):
-        self.__url = r"https://api.gochati.cn/jsapi.php?token=cxmooc&q="
+        self.__api1 = r"https://api.gochati.cn/jsapi.php?token=cxmooc&q="
+        self.__api2 = r"https://api.julym.com/class/damn.php?question="
 
-    def __requestAnswer(self, problem):
-        url = "{}{}".format(self.__url, problem)
+    def __requestAnswerToAPI1(self, problem):
+        url = "{}{}".format(self.__api1, problem)
         r = requests.get(url, timeout=10)
         dataText = r.text
-        # print(dataText)
         dataJson = json.loads(dataText)
         data = dataJson["da"]
+        return data
+
+    def __requestAnswerToAPI2(self, problem):
+        url = "{}{}".format(self.__api2, problem)
+        r = requests.get(url, timeout=10)
+        dataText = r.text
+        dataJson = json.loads(dataText)
+        data = dataJson['answer'].encode().decode()
+        return data
+
+    def __requestAnswer(self, problem):
+        key = False
+        data = ""
+        try:
+            data = self.__requestAnswerToAPI1(problem)
+        except requests.exceptions.ReadTimeout:
+            print("接口1查询失败")
+            key = True
+        if data == "" or key:
+            data = self.__requestAnswerToAPI2(problem)
         return data
 
     @staticmethod
@@ -71,10 +91,11 @@ class GetAnswer:
 人与社会\u0001人与自然\u0001人与人
 使生态文明建设的战略地位更加明确#使中国特色社会主义事业总体布局更加完善#深化了对党执政规律、社会主义建设规律、人类社会发展规律的认识#有利于夺取中国特色社会主义新胜利
 
-https://api.gochati.cn/jsapi.php?token=cxmooc&q=【多选题】马克思确认社会生活在本质上是实践的,也就是把社会生活“当作实践去理解”。社会生活的实践性体现在( )
+https://api.gochati.cn/jsapi.php?token=cxmooc&q=近平主席提出的“新时代”之“新”,是基于中国特色社会主义进入一个新的发展阶段
 http://api.muketool.com/notice?script=习近平主席提出的“新时代”之“新”,是基于中国特色社会主义进入一个新的发展阶段。&version=1.0.7
+https://api.julym.com/class/damn.php?question=
 """
 
 if __name__ == "__main__":
     a = GetAnswer()
-    print(a.getAnswer('【单选题】实践的主体是( )'))
+    print(a.getAnswer('【单选题】辩证法三大规律均是对“发展”的观点进行深化和拓展,其中“量变质变规律”解释了事物发展的( )'))
